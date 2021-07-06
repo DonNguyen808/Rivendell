cb308ddf29ce669fca0cbe8ae1c61ddbcc737137
DonNguyen808 <DonNguyen808@gmail.com>
2021-07-06 14:58:25 -0400

    first heroku push

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rivendell_django.wsgi
--- rivendell_django/settings.py
+++ rivendell_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -25,9 +26,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-(m_$i7oz2s0_*uj5sg6%4*)38gr+7e0rx)6h(7q#opss3*b*_#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rivendellgardens.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -121,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'),)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
